a8d9d42f3939aab235bac2402c66a37672a2734f
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-03-24 11:26:35 -0300

    Add project files.

---
+++ MVCMovies/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCMovies.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WelcomeMF(string name, int id)
+        {
+            ViewData["Title"] = "Welcome MF";
+            
+            return View();
+            
+        }
+    }
+    // => $"Hello, Welcome {name}! You're MotherFucker number {number}";//parameters to html on the address bar is, first "?" and the others are "&".
+}
